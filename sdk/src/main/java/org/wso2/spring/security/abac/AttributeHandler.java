4fd7441ecbdb4be1732f65cfd037f77bfb558b8e
rnavagamuwa <randikanavagamuwa@gmail.com>
2019-03-25 18:09:56 +0530

    Adding the initial version of EntitlementServices API support

--- sdk/src/main/java/org/wso2/spring/security/abac/AttributeHandler.java
+++ sdk/src/main/java/org/wso2/spring/security/abac/AttributeHandler.java
@@ -1,8 +1,10 @@
 package org.wso2.spring.security.abac;
 
 import org.json.JSONObject;
+import org.wso2.spring.security.abac.soaputils.wsdl.EntitledResultSetDTO;
 
 import java.util.Optional;
+import javax.xml.bind.JAXBElement;
 
 /**
  * @author Randika Navagamuwa
@@ -12,4 +14,7 @@ public interface AttributeHandler {
     boolean authorize(String policyRequest);
 
     Optional<JSONObject> getApiResourceList();
+
+    JAXBElement<EntitledResultSetDTO> getEntitledAttributes(String subjectName, String resourceName, String subjectId, String action,
+                                                            boolean enableChildSearch);
 }
--- sdk/src/main/java/org/wso2/spring/security/abac/XacmlAttributeHandler.java
+++ sdk/src/main/java/org/wso2/spring/security/abac/XacmlAttributeHandler.java
@@ -1,11 +1,17 @@
 package org.wso2.spring.security.abac;
 
+import org.apache.http.Header;
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,21 +19,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 import org.wso2.spring.security.abac.cache.CacheManager;
 import org.wso2.spring.security.abac.cache.EhCacheManager;
 import org.wso2.spring.security.abac.exception.AttributeEvaluatorException;
+import org.wso2.spring.security.abac.soaputils.CustomSSLHttpClientFactory;
+import org.wso2.spring.security.abac.soaputils.EntitlementServiceClient;
+import org.wso2.spring.security.abac.soaputils.wsdl.EntitledResultSetDTO;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import javax.net.ssl.SSLContext;
+import javax.xml.bind.JAXBElement;
 
 /**
  * @author Randika Navagamuwa
@@ -37,14 +51,19 @@ public class XacmlAttributeHandler implements AttributeHandler {
 
     private static String XACML_PDP_AUTHORIZE_URL;
     private static String XACML_PDP_RESOURCE_LIST_URL;
+    private static String XACML_PDP_ENTITLEMENT_SERVICE_URL;
     private static String TRUST_STORE;
     private static String TRUST_STORE_PASSWORD;
     private static String KEY_STORE;
     private static String KEY_STORE_PASSWORD;
+    private static String CERT_ALIAS;
 
     private CacheManager responseCacheManager;
     private SSLContext sslContext;
-    private HttpHeaders headers;
+    private HttpHeaders restHeaders;
+    private List<Header> soapHeaders;
+    private CustomSSLHttpClientFactory customSSLHttpClientFactory;
+    private EntitlementServiceClient entitlementServiceClient;
 
     public XacmlAttributeHandler() {
 
@@ -52,11 +71,14 @@ public class XacmlAttributeHandler implements AttributeHandler {
             Properties properties = PropertiesLoaderUtils
                     .loadAllProperties("application.properties");
             XACML_PDP_AUTHORIZE_URL = properties.getProperty("xacml.pdp.url.authorize");
-            XACML_PDP_RESOURCE_LIST_URL = properties.getProperty("xacml.pdp.url.resourceList");
+            XACML_PDP_RESOURCE_LIST_URL = properties.getProperty("xacml.pdp.url.resource.list");
+            XACML_PDP_ENTITLEMENT_SERVICE_URL = properties.getProperty("xacml.pdp.url.entitlement.service");
             TRUST_STORE = properties.getProperty("xacml.pdp.trustStore");
             TRUST_STORE_PASSWORD = properties.getProperty("xacml.pdp.trustStore.password");
             KEY_STORE = properties.getProperty("xacml.pdp.keyStore");
             KEY_STORE_PASSWORD = properties.getProperty("xacml.pdp.keyStore.password");
+            CERT_ALIAS = properties.getProperty("xacml.pdp.keyStore.cert.alias");
+
         } catch (IOException e) {
 
             //todo stop the whole app
@@ -75,18 +97,46 @@ public class XacmlAttributeHandler implements AttributeHandler {
                     .loadTrustMaterial(ResourceUtils.getFile("classpath:".concat(TRUST_STORE)),
                             TRUST_STORE_PASSWORD.toCharArray())
                     .build();
+
+            this.customSSLHttpClientFactory = new CustomSSLHttpClientFactory(
+                    new FileSystemResource(ResourceUtils.getFile("classpath:".concat(KEY_STORE))),
+                    KEY_STORE_PASSWORD,
+                    "JKS",
+                    new FileSystemResource(ResourceUtils.getFile("classpath:".concat(TRUST_STORE))),
+                    TRUST_STORE_PASSWORD,
+                    new String[]{"TLSv1"},
+                    CERT_ALIAS);
+
+            this.soapHeaders = new ArrayList<>();
+            //todo use mutual SSL
+            soapHeaders.add(new BasicHeader("Authorization", "Basic YWRtaW46YWRtaW4="));
+
+            this.responseCacheManager = new EhCacheManager();
+
+            Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+            marshaller.setContextPath("org.wso2.spring.security.abac.soaputils.wsdl");
+
+            this.entitlementServiceClient = new EntitlementServiceClient(XACML_PDP_ENTITLEMENT_SERVICE_URL);
+            this.entitlementServiceClient.setMarshaller(marshaller);
+            this.entitlementServiceClient.setUnmarshaller(marshaller);
+            this.entitlementServiceClient.setMessageSender(new HttpComponentsMessageSender(
+                    HttpClientBuilder
+                            .create()
+                            .setSSLSocketFactory(new SSLConnectionSocketFactory(this.sslContext, NoopHostnameVerifier.INSTANCE))
+                            .addInterceptorFirst(new HttpComponentsMessageSender.RemoveSoapHeadersInterceptor())
+                            .setDefaultHeaders(this.soapHeaders)
+                            .build()));
         } catch (Exception e) {
 
             //todo stop the whole app
             throw new AttributeEvaluatorException("Failed to read keystore or truststore", e);
         }
 
-        this.headers = new HttpHeaders();
-        this.headers.setContentType(MediaType.APPLICATION_JSON);
-        this.headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        this.headers.set("WSO2-Identity-User", "admin");
+        this.restHeaders = new HttpHeaders();
+        this.restHeaders.setContentType(MediaType.APPLICATION_JSON);
+        this.restHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        this.restHeaders.set("WSO2-Identity-User", "admin");
 
-        this.responseCacheManager = new EhCacheManager();
     }
 
     @Override
@@ -104,7 +154,7 @@ public class XacmlAttributeHandler implements AttributeHandler {
                     new HttpComponentsClientHttpRequestFactory(client));
             RestTemplate rt = restTemplateBuilder.build();
 
-            HttpEntity<String> entity = new HttpEntity<>(authRequest, this.headers);
+            HttpEntity<String> entity = new HttpEntity<>(authRequest, this.restHeaders);
 
             ResponseEntity response = rt.postForEntity(XACML_PDP_AUTHORIZE_URL, entity, String.class);
             if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
@@ -145,7 +195,7 @@ public class XacmlAttributeHandler implements AttributeHandler {
                     new HttpComponentsClientHttpRequestFactory(client));
             RestTemplate rt = restTemplateBuilder.build();
 
-            HttpEntity<String> entity = new HttpEntity<>(this.headers);
+            HttpEntity<String> entity = new HttpEntity<>(this.restHeaders);
 
             ResponseEntity response = rt.getForEntity(XACML_PDP_RESOURCE_LIST_URL, String.class, entity);
 
@@ -157,6 +207,16 @@ public class XacmlAttributeHandler implements AttributeHandler {
             this.responseCacheManager.putIfAbsent(XACML_PDP_RESOURCE_LIST_URL, cachedResponse);
         }
         return Optional.of(new JSONObject(cachedResponse));
+    }
+
+    @Override
+    public JAXBElement<EntitledResultSetDTO> getEntitledAttributes(String subjectName, String resourceName,
+                                                                   String subjectId, String action,
+                                                                   boolean enableChildSearch) {
+
+        return this.entitlementServiceClient.
+                getEntitledAttributes(subjectName, resourceName, subjectId, action, enableChildSearch).getReturn();
+
     }
 
     private KeyStore loadPfx(String file, char[] password) throws Exception {
